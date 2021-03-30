2060467a413a70e765e47c555e110bd45a55d312
Lars Kellogg-Stedman <lars@redhat.com>
2021-03-30 16:53:49 -0400

    Add a copy cards workflow with manual trigger
    Allows running the 'copy-cards' command via the "Run workflow" in
    GitHub.

--- moc_sprint_tools/cmd/copy_cards.py
+++ moc_sprint_tools/cmd/copy_cards.py
@@ -2,7 +2,7 @@ import click
 import github
 import logging
 
-from moc_sprint_tools.sprintman import BoardNotFoundError
+from moc_sprint_tools.sprintman import sort_sprints, BoardNotFoundError
 from moc_sprint_tools import defaults
 
 LOG = logging.getLogger(__name__)
@@ -31,13 +31,24 @@ def main(ctx, src, dst, copy_columns, ignore_columns, no_default_ignore):
     if ignore_columns is None and no_default_ignore:
         ignore_columns = []
 
+    sprints = sort_sprints(api.open_sprints)
+
+    def resolve_sprint(name):
+        if name.lower() in ['cur', 'current', 'latest']:
+            return sprints[-1]
+        elif name.lower() in ['prev', 'previous']:
+            return sprints[-2]
+        else:
+            return api.get_sprint(name)
+
     try:
-        src_board = api.get_sprint(src)
-        dst_board = api.get_sprint(dst)
+        src_board = resolve_sprint(src)
+        dst_board = resolve_sprint(dst)
     except BoardNotFoundError as err:
         raise click.ClickException(f'no board named {err}')
 
     LOG.info('copying cards from %s -> %s', src_board.name, dst_board.name)
+
     api.copy_board(src_board, dst_board,
                    columns=copy_columns,
                    ignore_columns=ignore_columns)
